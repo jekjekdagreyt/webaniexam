f3febda83aa90841ba6f7f46f53c3c110841c8ac
jekjekdagreyt <60175144+jekjekdagreyt@users.noreply.github.com>
2020-10-10 15:24:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Web Animation Exam!</title>
+	<script src="https://kit.fontawesome.com/662bc276a7.js" crossorigin="anonymous"></script>
+</head>
+<body>
+		<header>
+			<div class = "container-top">
+				<div class = "ncenter">
+					<div class="bawbaw"></div>
+					<div class="bawbawz"></div>
+					<div class="bawbawx"></div>
+					<div class="karabaw"></div>
+					<div class="bigotilyo"></div>
+				</div>
+				<div class="wrapper">
+					<div class = "neon-wrapper">
+						<span class = "text">Jake Nemenzo</span>
+						<span class="gradient"></span>
+						<span class = "dodge"></span>
+					</div>
+
+				</div>
+			</div>
+		</header>
+			<div class = "container-mid">
+				<div class = "slidershow middle">
+					<div class = "slides">
+						<input type = "radio" name = "r" id= "r1" checked>
+						<input type = "radio" name = "r" id= "r2">
+						<input type = "radio" name = "r" id= "r3">
+						<input type = "radio" name = "r" id= "r4">
+						<input type = "radio" name = "r" id= "r5">
+						
+						<div class = "slide s1">
+							<img src = "1.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "2.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "3.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "4.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "5.jpg" alt = "">
+						</div>
+					</div>	
+						<div class = "navb">
+							<label for= "r1" class="bar"></label>
+							<label for= "r2" class="bar"></label>
+							<label for= "r3" class="bar"></label>
+							<label for= "r4" class="bar"></label>
+							<label for= "r5" class="bar"></label>
+						</div>
+				</div>
+				<div class = "infor">
+					<div class="kaliwa">
+						<h1 class = hijoz>
+							ABOUT ME : 
+						</h1>
+					</div>
+					<div class="kanan">
+						<h1 class = "hijo">
+							20 YEARS OLD!
+						</h1>
+						<hr class = "opya">
+						<h1 class = "hijo">
+							I'm awesome
+						</h1>
+					</div>
+				</div>
+				<div class = "portfolio">
+					<div class = "upperp"><h1 class ="hijo">Portfolio! : </h1></div>
+					<div class ="lowerp">
+						<div class = "button1">
+							<a href = "svg/index.html" target="_blank" class ="hijoi">Learning Task 5.1</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		<footer>
+			<div class = "container-bottom">
+				<div class = "half"></div>
+				<div class="sentro">
+					<div class = "left">
+						<div class = "bungo">
+							<div class="ha"></div>
+							<div class="he"></div>
+							<div class="hi"></div>
+							<div class="ho"></div>
+							<div class="hu"></div>
+						</div>
+					</div>
+					<div class = "right">
+						<ul>
+							<li class = "icons">
+								<a href = "https://www.facebook.com/Jekjekdagreyt" target="_blank">
+									<i class="fab fa-facebook-square"></i>
+								</a>
+							</li>
+							<li class = "icons">
+								<a href = "https://www.instagram.com/jak.kewl/" target="_blank">
+									<i class="fab fa-instagram-square"></i>
+								</a>
+							</li>
+							<li class = "icons">
+								<a href = "https://codepen.io/jake-nemenzo" target="_blank">
+									<i class="fab fa-codepen"></i>
+								</a>
+							</li>
+						</ul>
+						<hr class = "opya">
+						<h3 class = "jek">By : Jake Nemenzo</h3>
+					</div>
+				</div>
+			</div>
+
+		</footer>
+</body>
+</html>
