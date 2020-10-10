f718401d028bce7a702b7551402f70c39ad1298d
jekjekdagreyt <60175144+jekjekdagreyt@users.noreply.github.com>
2020-10-10 15:22:03 +0800

    Add files via upload

--- svg/index.php
+++ svg/index.php
@@ -1,1 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+	<div id = "container">
+		<div class = "first">
+	  <svg class = "x">
+	     <line x1="0" y1="0" x2="500" y2="32" stroke-width="25" stroke="#F763B5"/>
+	      <line x1="-10" y1="30" x2="700" y2="32" stroke-width="25" stroke="orange"/>
+	      <line x1="0" y1="50" x2="400" y2="70" stroke-width="25" stroke="blue"/>
+	      <line x1="-10" y1="82" x2="700" y2="32" stroke-width="25" stroke="green"/>
+	      <line x1="-100" y1="90" x2="200" y2="100" stroke-width="25" stroke="red"/>
+	      <line x1="0" y1="100" x2="0" y2="400" stroke-width="200" stroke="yellow"/>
+	      <line x1="100" y1="200" x2="200" y2="200" stroke-width="200" stroke="skyblue"/>
+	     </svg>
+		</div>
+		<div class="second">
+		<svg class="x">	
+			<circle cx="20" cy="20" r="10" fill="#FE0000" stroke-width="1" stroke=""/>
+			<circle cx="75" cy="35" r="15" fill="#FE3A00" stroke-width="1" stroke=""/>
+			<circle cx="145" cy="45" r="20" fill="#FE5C00" stroke-width="1" stroke=""/>
+			<circle cx="145" cy="135" r="45" fill="#FE7F00" stroke-width="1" stroke=""/>
+			<circle cx="145" cy="225" r="20" fill="#FEAD00" stroke-width="1" stroke=""/>
+			<circle cx="75" cy="235" r="15" fill="#FECF00" stroke-width="1" stroke=""/>
+			<circle cx="20" cy="250" r="10" fill="#FEF600" stroke-width="1" stroke=""/>
+		</svg>
+	</div>
+		<div class="third">
+			<svg class="x">
+				<rect width="100" height="300" fill="red" stroke-width="0" stroke="rgb(0,0,0)"/>
+				<line x1="125" y1="70" x2="0" y2="0" stroke-width="100" stroke="white"/>
+				<line x1="75" y1="70" x2="200" y2="0" stroke-width="100" stroke="white"/>
+				<circle cx="100" cy="40" r="20" fill="yellow" stroke-width="1" stroke=""/>
+				<circle cx="99" cy="95" r="10" fill="yellow" stroke-width="1" stroke=""/>
+				<circle cx="185" cy="15" r="10" fill="yellow" stroke-width="1" stroke=""/>
+				<circle cx="15" cy="15" r="10" fill="yellow" stroke-width="1" stroke=""/>
 
+			</svg>
+		</div>
+		<div class="fourth">
+			<svg class="x">
+				<line x1="0" y1="300" x2="0" y2="0" stroke-width="100" stroke="#04F89A"/>
+				<line x1="100" y1="300" x2="100" y2="0" stroke-width="50" stroke="#15C5EE"/>
+				<line x1="175" y1="300" x2="175" y2="0" stroke-width="50" stroke="#CCF804"/>
+				<circle cx="137" cy="0" r="10" fill="red" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="30" r="10" fill="orange" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="60" r="10" fill="yellow" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="90" r="10" fill="green" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="120" r="10" fill="blue" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="150" r="10" fill="#3504F8" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="180" r="10" fill="violet" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="210" r="10" fill="black" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="240" r="10" fill="skyblue" stroke-width="1" stroke=""/>
+				<circle cx="62" cy="270" r="10" fill="orangered" stroke-width="1" stroke=""/>
+				<circle cx="137" cy="300" r="10" fill="yellowgreen" stroke-width="1" stroke=""/>
+			</svg>
+		</div>
+		<div class="fifth">
+			<svg class="x">
+				<rect x="0" y="250" width="200" height="50" fill="#4F3117" stroke-width="0" stroke="white"/>
+				<rect x="75" y="20" width="50" height="250" fill="#6A370A" stroke-width="0" stroke="white"/>
+				<rect x="25" y="70" width="150" height="50" fill="#6A370A" stroke-width="3" stroke="white"/>
+				<circle cx="100" cy="80" r="15" fill="yellowgreen" stroke-width="1" stroke=""/>
+				<ellipse cx="135" cy="100" rx="30" ry="5" fill="yellowgreen" stroke-width="1" stroke=""/>
+				<ellipse cx="65" cy="100" rx="30" ry="5" fill="yellowgreen" stroke-width="1" stroke=""/>
+				<ellipse cx="100" cy="140" rx="20" ry="50" fill="yellowgreen" stroke-width="1" stroke=""/>
+
+			</svg>
+		</div>
+		<div class="sixth">
+			<svg class="x">
+				<circle cx="165" cy="55" r="60" fill="#E5E5E5"/>
+				<rect x="" y="210" width="200" height="90" fill="#1F1A46" transform="skewX(30) skewY(0)"/>
+				<rect x="" y="210" width="200" height="80" fill="#1F1A46" transform="skewX(-35) skewY(0)"/>
+				<rect x="20" y="90" width="40" height="120"  stroke-width="1" stroke="white"fill="black"/>
+				<rect x="40" y="130" width="50" height="5" fill="black"/>
+				<rect x="40" y="110" width="50" height="5" fill="black"/>
+				<rect x="80" y="90" width="40" height="120"  stroke-width="1" stroke="white"fill="black"/>
+				<rect x="" y="110" width="50" height="100" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="52" y="170" width="50" height="40" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="104" y="140" width="50" height="70" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="156" y="125" width="50" height="85" stroke-width="1" stroke="white" fill="black"/>
+				<rect x="10" y="130" width="10" height="10" fill="yellow"/>
+				<rect x="30" y="130" width="10" height="10" fill="yellow"/>
+				<rect x="10" y="160" width="10" height="10" fill="yellow"/>
+				<rect x="30" y="160" width="10" height="10" fill="yellow"/>
+				<rect x="60" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="80" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="115" y="150" width="10" height="10" fill="yellow"/>
+				<rect x="135" y="150" width="10" height="10" fill="yellow"/>
+				<rect x="115" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="135" y="180" width="10" height="10" fill="yellow"/>
+				<rect x="165" y="145" width="10" height="10" fill="yellow"/>
+				<rect x="185" y="145" width="10" height="10" fill="yellow"/>
+				<rect x="165" y="165" width="10" height="10" fill="yellow"/>
+				<rect x="185" y="165" width="10" height="10" fill="yellow"/>
+				<circle cx="115" cy="260" r="50" fill="#E5E5E5" transform="skewX(20)"/>
+			</svg>
+		</div>
+		<div class="seventh">
+			<svg class="x">
+				<rect x="-162" y="60" width="250" height="50" fill="white" transform="rotate(-65)"/>
+				<rect x="0" y="145" width="100" height="50" fill="white" transform=""/>
+				<rect x="0" y="-120" width="260" height="50" fill="white" transform="rotate(70)"/>
+				<rect x="145" y="195" width="70" height="50" fill="red" transform=""/>
+				 <text x="5" y="250" font-family="Consolas" font-size="70" fill="white">Adobe</text>
+				<circle cx="25" cy="275" r="10" fill="white"/>
+				<circle cx="65" cy="275" r="10" fill="white"/>
+				<circle cx="102" cy="275" r="10" fill="white"/>
+				<circle cx="139" cy="275" r="10" fill="white"/>
+				<circle cx="175" cy="275" r="10" fill="white"/>
+			</svg>
+		</div>
+</div>
+</body>
+</html>
