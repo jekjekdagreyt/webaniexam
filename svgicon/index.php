10213c49ea44433151c281177d9f294a0947f110
jekjekdagreyt <60175144+jekjekdagreyt@users.noreply.github.com>
2020-10-26 11:06:17 +0800

    Create index.php

---
+++ svgicon/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>SVG ICON</title>
+</head>
+<body>
+	<svg id="svg-container">
+		<g>
+			<ellipse cx="80" cy="270" rx="20" ry="4" stroke-width="2" fill="yellow" transform=""/>
+		 	<ellipse cx="200" cy="270" rx="30" ry="5" stroke-width="2" fill="#842FD8" transform=""/>
+		 	<ellipse cx="140" cy="250" rx="50" ry="8" stroke-width="2" fill="#5BE818" transform=""/>
+		</g>
+		<g>
+		 <path d="M150 270 L90 150 L205 150 Z" stroke="#854A1B" fill="#854A1B"/>
+		 <line x1="105" y1="160" x2="155" y2="250" stroke-width="2" stroke="#CB8851"/>
+		 <line x1="125" y1="200" x2="145" y2="160" stroke-width="2" stroke="#CB8851"/>
+		 <line x1="135" y1="225" x2="165" y2="160" stroke-width="2" stroke="#CB8851"/>
+		 <line x1="145" y1="250" x2="185" y2="160" stroke-width="2" stroke="#CB8851"/>
+		 <line x1="120" y1="160" x2="165" y2="225" stroke-width="2" stroke="#CB8851"/>
+		 <line x1="170" y1="200" x2="145" y2="160" stroke-width="2" stroke="#CB8851"/>
+			<g fill="#5BE818">
+				<circle cx="145" cy="85" r="55"/>
+				<ellipse cx="125" cy="70" rx="25" ry="4" stroke-width="2" transform="rotate(-10)" fill="white"/>
+			</g>
+		 <g fill="#F784AA">
+		 	<circle cx="145" cy="110" r="40"/>
+		 	<ellipse cx="130" cy="100" rx="15" ry="2" stroke-width="2" transform="rotate(-5)" fill="white"/>
+		 	<circle cx="128" cy="135" r="15"/>
+		 	<circle cx="145" cy="142" r="15"/>
+		 	<circle cx="163" cy="137" r="15"/>
+		</g>
+
+		<g fill="yellow">
+			<circle cx="90" cy="120" r="40"/>
+			<ellipse cx="65" cy="110" rx="20" ry="3" stroke-width="2" transform="rotate(-8)" fill="white"/>
+			<circle cx="80" cy="150" r="15"/>
+			<circle cx="102" cy="147" r="15"/>
+			<circle cx="120" cy="135" r="15"/>
+		</g>
+		 
+		 <g fill="#842FD8">
+		 	<circle cx="195" cy="115" r="40"/>
+		 	<ellipse cx="180" cy="110" rx="20" ry="3" stroke-width="2" transform="rotate(-6)" fill="white"/>
+		 	<circle cx="165" cy="130" r="15"/>
+		 	<circle cx="180" cy="145" r="15"/>
+		 	<circle cx="205" cy="148" r="15"/>
+		 </g>
+
+		 <g>
+		 	<ellipse cx="80" cy="165" rx="5" ry="8" fill="yellow">
+		 		<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="0 0;0 100;"
+					dur=".9s"
+					repeatCount="indefinite"/>
+		 	</ellipse>
+		 	<ellipse cx="78" cy="165" rx="1" ry="3" fill="white">
+		 		<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="0 0;0 100;"
+					dur=".9s"
+					repeatCount="indefinite"/>
+		 	</ellipse>
+			<ellipse cx="210" cy="165" rx="5" ry="8" fill="#842FD8">
+				<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="0 0;0 100;"
+					dur="1.2s"
+					repeatCount="indefinite"/>
+			</ellipse>
+			
+		 </g>
+	</g>
+		
+
+	</svg>
+</body>
+</html>
