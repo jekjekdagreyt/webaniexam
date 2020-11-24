f79337de2b6be0d1bca31836605be84cc79213ec
jekjekdagreyt <60175144+jekjekdagreyt@users.noreply.github.com>
2020-11-24 10:22:15 +0800

    Create index.php

---
+++ floomsvg/index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Floom</title>
+</head>
+<body>
+	<div id="header">
+		<div class = "navi">
+			<a href ="https://www.floom.com/" >
+				<g>
+				<svg class = "nav">
+					<rect x="-11" y="18" width="30" height="5" fill="#474344" transform="rotate(-40)"/>
+					<rect x="20" y="-15" width="30" height="5" fill="#474344" transform="rotate(40)"/>
+					<rect x="10" y="40" width="30" height="5" fill="#474344" stroke-width=""/>
+					<rect x="10" y="15" width="5" height="30" fill="#474344"/>
+					<rect x="36" y="15" width="5" height="30" fill="#474344"/>
+					<rect x="18" y="22" width="15" height="3" fill="#474344"/>
+				</svg>
+			</g>
+		</a>
+		<h3 class="h3">Home</h3>
+		</div>
+		<div class = "logox">		
+		<svg class = "logoxx">
+			<g>
+				<text x="15" y="30" font-family="Arial" font-size="25" fill="black">F</text>
+				<text x="40" y="30" font-family="Arial" font-size="25" fill="black">L</text>
+				<text x="60" y="30" font-family="Arial" font-size="25" fill="black">O</text>
+				<text x="85" y="20" font-family="Arial" font-size="25" fill="black">O</text>
+				<text x="110"y="30" font-family="Arial" font-size="25" fill="black">M</text>
+			</g>
+		</svg>
+		
+		</div>
+	</div>
+	<div id = "body">
+		<div class = "flowers">
+			<svg class = "flowerpot1">
+				<g>
+					<circle cx="1380" cy="90" r="100" fill="yellow"/>
+				</g>
+				<g>
+				<ellipse cx="100" cy="100" rx="50" ry="20" fill="white">
+				<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="1024 0;0 0;"
+					dur="120s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="140" cy="80" rx="50" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="1024 0;0 0;"
+					dur="120s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="150" cy="100" rx="50" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="1024 0;0 0;"
+					dur="120s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="180" cy="90" rx="50" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="1024 0;0 0;"
+					dur="120s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				</g>
+
+
+				<g>
+				<ellipse cx="700" cy="50" rx="70" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="2000 0;0 0;"
+					dur="80s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="740" cy="30" rx="80" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="2000 0;0 0;"
+					dur="80s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="750" cy="70" rx="90" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="2000 0;0 0;"
+					dur="80s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="780" cy="60" rx="70" ry="20" fill="white">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="2000 0;0 0;"
+					dur="80s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				</g>
+
+
+				
+				<g>
+					<ellipse cx="600" cy="300" rx="100" ry="30" fill="white"/>
+					<ellipse cx="640" cy="270" rx="100" ry="30" fill="white"/>
+					<ellipse cx="700" cy="240" rx="100" ry="30" fill="white"/>
+					<ellipse cx="740" cy="270" rx="100" ry="30" fill="white"/>
+					<ellipse cx="800" cy="300" rx="100" ry="30" fill="white"/>
+					<ellipse cx="700" cy="300" rx="100" ry="30" fill="white"/>
+				</g>
+
+				<g>
+					<ellipse cx="200" cy="100" rx="140" ry="30" fill="white">
+						<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="200 0;-2000 0;"
+					dur="60s"
+					repeatCount="indefinite"/>
+					</ellipse>
+					<ellipse cx="240" cy="70" rx="140" ry="30" fill="white">
+						<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="200 0;-2000 0;"
+					dur="60s"
+					repeatCount="indefinite"/>
+					</ellipse>
+					<ellipse cx="300" cy="40" rx="140" ry="30" fill="white">
+						<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="200 0;-2000 0;"
+					dur="60s"
+					repeatCount="indefinite"/>
+					</ellipse>
+					<ellipse cx="340" cy="70" rx="140" ry="30" fill="white">
+						<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="200 0;-2000 0;"
+					dur="60s"
+					repeatCount="indefinite"/>
+					</ellipse>
+					<ellipse cx="400" cy="100" rx="140" ry="30" fill="white">
+						<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="200 0;-2000 0;"
+					dur="60s"
+					repeatCount="indefinite"/>
+					</ellipse>
+					<ellipse cx="300" cy="100" rx="140" ry="30" fill="white">
+						<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="200 0;-2000 0;"
+					dur="60s"
+					repeatCount="indefinite"/>
+					</ellipse>
+				</g>
+
+				<g>
+					<rect x="100" y="1100" width="10" height="110" fill="green"/>
+					<ellipse cx="75" cy="1100" rx="30" ry="10" fill="yellow"/>
+					<ellipse cx="340" cy="1030" rx="30" ry="10" fill="yellow" transform="rotate(14)"/>
+					<ellipse cx="105" cy="1070" rx="10" ry="30" fill="yellow"/>
+					<ellipse cx="135" cy="1100" rx="30" ry="10" fill="yellow"/>
+					<ellipse cx="-250" cy="1060" rx="30" ry="10" fill="yellow" transform="rotate(-20)"/>
+					<circle cx="105" cy="1100" r="20" fill="#9F581C"/>
+				</g>
+				<g>
+					<rect x="500" y="480" width="10" height="200" fill="white"/>
+					<rect x="500" y="480" width="500" height="10" fill="white"/>
+					<rect x="990" y="480" width="10" height="200" fill="white"/>
+					<rect x="500" y="680" width="500" height="10" fill="white"/>
+					<text x="550" y="620" font-family="Arial" font-size="100" fill="white">F</text>
+					<text x="620" y="620" font-family="Arial" font-size="100" fill="white">L</text>
+					<text x="690" y="620" font-family="Arial" font-size="100" fill="white">O</text>
+					<text x="780" y="590" font-family="Arial" font-size="100" fill="white">O</text>
+					<text x="870" y="620" font-family="Arial" font-size="100" fill="white">M</text>
+				</g>
+			</svg>
+			<div class = "nge">
+				<svg class ="zz">
+					<rect x="42" y="148" width="10" height="120" fill="green"/>
+					<ellipse cx="40" cy="100" rx="20" ry="50" fill="red"/>
+					<ellipse cx="90" cy="80" rx="20" ry="50" fill="red" transform="rotate(15)"/>
+					<circle cx="50" cy="135" r="15" fill="red"/>
+				</svg>
+			</div>
+			<div class = "hat">
+				<svg class="zz">
+					<rect x="90" y="150" width="8" height="100" fill="Green"/>
+					<circle cx="60" cy="150" r="30" fill="green"/>
+					<circle cx="95" cy="110" r="30" fill="green"/>
+					<circle cx="125" cy="150" r="30" fill="green"/>
+					<circle cx="95" cy="165" r="30" fill="green"/>
+				</svg>
+			</div>
+			<div class = "hatdog">
+				<svg class="zx">
+				<rect x="140" y="-120" width="50" height="10" fill="#0044AA" transform="rotate(72)"/>
+				<rect x="138" y="-267" width="50" height="10" fill="#0044AA" transform="rotate(74)"/>
+				<rect x="110" y="142" width="150" height="10" fill="#0044AA" transform="rotate(-19)"/>
+				<rect x="130" y="120" width="120" height="20" fill="#0044AA" transform="rotate(16)"/>
+				<ellipse cx="75" cy="160" rx="20" ry="32" fill="#0044AA"/>
+				<ellipse cx="70" cy="170" rx="5" ry="8" fill="cyan">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="0 0;0 200;"
+					dur="1s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="70" cy="170" rx="5" ry="8" fill="cyan">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="0 0;0 200;"
+					dur=".8s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<ellipse cx="70" cy="150" rx="5" ry="8" fill="cyan">
+					<animateTransform attributeName="transform"
+					attributeType="XML"
+					type="translate"
+					values="0 0;0 200;"
+					dur=".5s"
+					repeatCount="indefinite"/>
+				</ellipse>
+				<rect x="120" y="180" width="150" height="150" fill="#0044AA" transform="rotate(-15)"/>
+			</svg>
+			</div>
+			<div class = "meh">
+				<svg class="mew">
+				<ellipse cx="100" cy="200" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="150" cy="140" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="240" cy="100" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="100" cy="170" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="330" cy="140" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="420" cy="200" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="250" cy="200" rx="100" ry="50" fill="green"/>
+			</svg>
+			</div>
+			<div class = "mex">
+				<svg class="mew">
+				<ellipse cx="100" cy="200" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="150" cy="140" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="240" cy="100" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="100" cy="170" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="330" cy="140" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="420" cy="200" rx="100" ry="50" fill="Green"/>
+				<ellipse cx="250" cy="200" rx="100" ry="50" fill="green"/>
+			</svg>
+			</div>
+			<div class = "b">
+				<svg class = "zxzx">
+							<rect x="42" y="148" width="10" height="60" fill="green"/>
+					<ellipse cx="42" cy="130" rx="8" ry="20" fill="red"/>
+					<ellipse cx="87" cy="110" rx="8" ry="20" fill="red" transform="rotate(15)"/>
+					<circle cx="48" cy="145" r="5" fill="red"/>
+				</svg>
+			</div>
+			<div class = "c">
+				<svg class = "zxzx">
+							<rect x="42" y="148" width="10" height="60" fill="green"/>
+					<ellipse cx="42" cy="130" rx="8" ry="20" fill="red">
+						
+					</ellipse>
+					<ellipse cx="87" cy="110" rx="8" ry="20" fill="red" transform="rotate(15)">
+						
+					</ellipse>
+					<circle cx="48" cy="145" r="5" fill="red">
+						
+					</circle>
+				</svg>
+			</div>
+		</div>
+	</div>
+	<div id = "footer">
+		
+		<svg class = "footlogo">
+			<g>
+				<text x="15" y="30" font-family="Arial" font-size="25" fill="white">F</text>
+				<text x="40" y="30" font-family="Arial" font-size="25" fill="white">L</text>
+				<text x="60" y="30" font-family="Arial" font-size="25" fill="white">O</text>
+				<text x="85" y="23" font-family="Arial" font-size="25" fill="white">O</text>
+				<text x="110"y="30" font-family="Arial" font-size="25" fill="white">M</text>
+			</g>
+		</svg>
+		<svg class ="haha">
+			<g>
+				<text x="15" y="25" font-family="Arial" font-size="12" fill="black">© 2020 / FLOOM. LTD / ALL RIGHTS RESERVED</text>
+			</g>
+
+		</svg>
+	
+	</div>
+</body>
+</html>
